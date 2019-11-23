d19ac0e6f361f100e1268fd653e19a1079d8060d
Ricky Kazuo Miller <ricky@ricky.me>
2019-11-24 01:48:04 +0900

    exit with code 1 if there is an error

--- run.sh
+++ run.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 while read digest; do
-  echo "$digest"
   source=gcr.io/cloud-builders/gcloud@$digest
   target=docker.pkg.github.com/$GITHUB_REPOSITORY/gcloud
-  docker pull $source
-  docker tag $source $target
-  docker push $target
+  docker pull $source || exit 1
+  docker tag $source $target || exit 1
+  docker push $target || exit 1
 done <digests.txt
